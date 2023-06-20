af03c97b949368c935200cc51b9b52c48b985a4b
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-06-20 17:38:06 +0900

    📑 :: customAxios 작성 및 토큰 레포지토리 쿼리 생성

---
+++ src/lib/Axios/responseInterceptor.ts
@@ -0,0 +1,43 @@
+import { B1ndToast } from "@b1nd/b1nd-toastify";
+import { AxiosError } from "axios";
+import {
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+  REQUEST_TOKEN_KEY,
+} from "../../constants/Token/Token.constant";
+import Token from "../Token/Token";
+import { customAxios } from "./customAxios";
+import TokenRepositoryImpl from "../../repositories/TokenRepositoty/TokenRepositoryImpl";
+
+export const responseErrorInterceptor = async (error: AxiosError) => {
+  if (error.response) {
+    const {
+      response: { status },
+    } = error;
+
+    const usingAccessToken = Token.get(ACCESS_TOKEN_KEY);
+    const usingRefreshToken = Token.get(REFRESH_TOKEN_KEY);
+
+    if (
+      usingAccessToken !== undefined &&
+      usingRefreshToken !== undefined &&
+      status === 401
+    ) {
+      try {
+        const { data: newAccessToken } = await TokenRepositoryImpl.postRefresh({
+          token: usingRefreshToken,
+        });
+
+        Token.set(ACCESS_TOKEN_KEY, newAccessToken);
+        customAxios.defaults.headers.common[
+          REQUEST_TOKEN_KEY
+        ] = `Bearer ${newAccessToken}`;
+      } catch (error) {
+        B1ndToast.showError("세션이 만료 되었습니다.");
+        window.location.href = "/teacher";
+      }
+    }
+  }
+
+  return Promise.reject(error);
+};
